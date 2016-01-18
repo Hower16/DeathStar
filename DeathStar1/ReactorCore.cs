530a50dc6ed4bed1844c9270664595056206a039
Miky1216 <michael.s.terrill@gmail.com>
2016-01-18 17:15:19 -0600

    commit to pull

--- DeathStar1/ReactorCore.cs
+++ DeathStar1/ReactorCore.cs
@@ -3,12 +3,30 @@
     public class ReactorCore
     {
         int fuelRods;
+        int fueldRodLevel;
         int controlRods;
+        int controlRodLevel;
         int waterReactorLevel;
 
-        public ReactorCore()
+        public ReactorCore(int FuelRods, int FuelRodLevel, int ControlRods, int ControlRodLevel, int WaterReactorLevel)
         {
+            fuelRods = FuelRods;
+            fueldRodLevel = FuelRodLevel;
+            controlRods = ControlRods;
+            controlRodLevel = ControlRodLevel;
+            waterReactorLevel = WaterReactorLevel;
+        }
 
+        public int CheckFuelRodLevel()
+        {
+            if (fueldRodLevel > 0)
+            {
+                return fueldRodLevel;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
